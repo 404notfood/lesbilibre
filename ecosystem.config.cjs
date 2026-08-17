c047f2accb3a9b0090023b00cb7c3cf7a9d76cff
Laurent <l.prado56920@gmail.com>
2026-08-17 14:56:18 +0200

    pull

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     apps: [
         {
             name: 'laravel-queue',
-            script: '/usr/bin/php',
+            script: 'php',
             args: 'artisan queue:listen --tries=3 --timeout=90 --sleep=3',
             interpreter: 'none',
             instances: 1,
@@ -15,23 +15,6 @@ module.exports = {
             },
             error_file: './storage/logs/queue-error.log',
             out_file: './storage/logs/queue-out.log',
-            log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-        },
-        {
-            name: 'laravel-reverb',
-            script: '/usr/bin/php',
-            args: 'artisan reverb:start --host=127.0.0.1 --port=6001',
-            interpreter: 'none',
-            instances: 1,
-            autorestart: true,
-            watch: false,
-            max_memory_restart: '512M',
-            kill_timeout: 10000,
-            env: {
-                NODE_ENV: 'production',
-            },
-            error_file: './storage/logs/reverb-error.log',
-            out_file: './storage/logs/reverb-out.log',
             log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
         },
     ],
